3d1cd0928d16b86e2b0df936ba97174b4e1eaad7
Mike <vukongminh@gmail.com>
2026-06-17 01:23:30 +0700

    1st code

---
+++ src/test/java/com/smart/dev/JavaToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
